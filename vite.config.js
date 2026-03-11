af191ae87d21d156644e4055747864c477c39199
WaseurGIT <waseurrahman31@gmail.com>
2026-03-11 06:14:09 +0600

    deploying in vercel attempt 10

--- vite.config.js
+++ vite.config.js
@@ -9,3 +9,13 @@ export default defineConfig({
     outDir: "dist",
   },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "dist",
+//   },
+// });
