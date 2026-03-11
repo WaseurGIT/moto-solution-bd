c1a7f3985dea80d03375f17a0b6da014ccf7f148
WaseurGIT <waseurrahman31@gmail.com>
2026-03-11 06:22:41 +0600

    deploying in vercel attempt 11

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist",
-  },
-});
-
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
 
+// // https://vite.dev/config/
 // export default defineConfig({
-//   plugins: [react()],
+//   plugins: [react(), tailwindcss()],
 //   build: {
 //     outDir: "dist",
 //   },
 // });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+});
